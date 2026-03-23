c08751a36bb79315fc9b3660b7a03a892d0d2a95
davii-lv <246378493+davii-lv@users.noreply.github.com>
2026-03-23 00:45:06 -0400

    add logo text in the menu

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
   import logo from '$lib/assets/logos/as-logo-symb-colour.svg';
+  import logoText from '$lib/assets/logos/as-logo-white-text.svg';
   import Link from './Link.svelte';
 </script>
 
 <div class="navbar absolute top-0 right-0 left-0 z-50 mx-auto max-w-5xl bg-transparent">
   <div class="navbar-start">
-    <Link href="/" class="p-1">
+    <Link href="/" class="flex items-center gap-3 p-1">
       <img src={logo} alt="Auspatious" class="h-10" />
+      <img src={logoText} alt="Auspatious" class="h-3" />
     </Link>
   </div>
   <div class="navbar-center"></div>
