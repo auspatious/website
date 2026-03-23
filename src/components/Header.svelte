75f7f02a78880aef9dbd3224383cf43348b425ba
davii-lv <246378493+davii-lv@users.noreply.github.com>
2026-03-22 23:51:04 -0400

    create helper <Link /> component to prefix href's with {base} for GH pages

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import logo from '$lib/assets/logos/as-logo-symb-colour.svg';
+  import Link from './Link.svelte';
 </script>
 
 <div class="navbar bg-transparent">
   <div class="navbar-start">
-    <a href="/">
+    <Link href="/">
       <img src={logo} alt="Auspatious" class="h-10" />
-    </a>
+    </Link>
   </div>
   <div class="navbar-center"></div>
   <div class="navbar-end"></div>
