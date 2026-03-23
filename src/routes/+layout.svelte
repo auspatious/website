fad9cf7d1fb88d849a208c94dc32db9ab058a0a8
davii-lv <246378493+davii-lv@users.noreply.github.com>
2026-03-22 22:58:30 -0400

    initialize state and add font switching functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { initializeAppState } from '$lib/state.svelte';
+
+	const app = initializeAppState();
+
+	$effect(() => {
+		document.documentElement.style.setProperty('--font-serif', app.serifFont);
+		document.documentElement.style.setProperty('--font-sans', app.sansSerifFont);
+	});
 
 	let { children } = $props();
 </script>
